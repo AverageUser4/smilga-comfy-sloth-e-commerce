be2f337c7e1226b04694a818de5eacbca2638877
Kacper <kacpersloka@o2.pl>
2022-12-15 14:31:36 +0100

    work on css

---
+++ src/components/ProductsFilterForm/ProductsFilterForm.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './ProductsFilterForm.module.css';
+
+function ProductsFilterForm(props) {
+  return (
+    <form className={css['products-form']}>
+
+      <input className="input" placeholder='Search'/>
+
+      <fieldset className="fieldset">
+        <legend className="heading heading--nano heading--no-margin">Category</legend>
+        <ul className={`list ${css['categories-list']}`}>
+          <li><button className="text-button">All</button></li>
+          <li><button className="text-button">Office</button></li>
+          <li><button className="text-button">Living room</button></li>
+          <li><button className="text-button">Kitchen</button></li>
+          <li><button className="text-button">Bedroom</button></li>
+          <li><button className="text-button">Dining room</button></li>
+          <li><button className="text-button">For kids</button></li>
+        </ul>
+      </fieldset>
+
+      <fieldset className="fieldset">
+        <legend className="heading heading--nano heading--no-margin">Company</legend>
+        <select className="input">
+          <option>All</option>
+          <option>Marcos</option>
+          <option>Liddy</option>
+          <option>Ikea</option>
+          <option>Caressa</option>
+        </select>
+      </fieldset>
+
+      <fieldset className="fieldset horizontal-list">
+        <legend className="heading heading--nano heading--no-margin">Colors</legend>
+        <label className={css['focus-label']}>
+          <input defaultChecked className="hidden-radio" type="radio" name="color" value=""/>
+          <span className="text-button">All</span>
+        </label>
+        <label className={css['focus-label']}>
+          <input className="hidden-radio" type="radio" name="color" value="red"/>
+          <span className="color"></span>
+        </label>
+        <label className={css['focus-label']}>
+          <input className="hidden-radio" type="radio" name="color" value="green"/>
+          <span className="color color--green"></span>
+        </label>
+        <label className={css['focus-label']}>
+          <input className="hidden-radio" type="radio" name="color" value="purple"/>
+          <span className="color color--blue"></span>
+        </label>
+        <label className={css['focus-label']}>
+          <input className="hidden-radio" type="radio" name="color" value="black"/>
+          <span className="color color--black"></span>
+        </label>
+        <label className={css['focus-label']}>
+          <input className="hidden-radio" type="radio" name="color" value="orange"/>
+          <span className="color color--orange"></span>
+        </label>
+      </fieldset>
+
+      <label className="vertical-list">
+        <span className="heading heading--nano heading--only-bottom-margin">Price</span>
+        <span className="block paragraph paragraph--1-line-height">$3100</span>
+        <input className="block range" type="range" min="0" max="3100"/>
+      </label>
+
+      <label className={css['free-shipping']}>
+        <span>Free shipping</span>
+        <input
+          id="freeShipping"
+          type="checkbox"
+        />
+      </label>
+
+      <button className="button button--danger button--little-padding">Clear Filters</button>
+
+    </form>
+  );
+}
+
+ProductsFilterForm.propTypes = {
+
+};
+
+export default ProductsFilterForm;
--- src/pages/AllProducts/AllProducts.js
+++ src/pages/AllProducts/AllProducts.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import CurrentPath from '../../components/CurrentPath/CurrentPath.js';
+import ProductsFilterForm from '../../components/ProductsFilterForm/ProductsFilterForm.js';
 import StandaloneSection from '../../components/StandaloneSection/StandaloneSection.js';
+import Grid from '../../components/Grid/Grid.js';
+import Product from '../../components/Product/Product.js';
+import Siblings from '../../components/Siblings/Siblings.js';
 
 // using fieldsets instead of sections makes more sense
 
@@ -11,81 +15,44 @@ function AllProducts() {
 
       <StandaloneSection>
 
-        <aside>
+        <Siblings>
 
-          <form>
+          <ProductsFilterForm/>
 
-            <input className="input" placeholder='Search'/>
+          <Grid>
+          <Product
+                name="sofa"
+                price={123}
+                image="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=400,height=400,fit=cover/animal/breed/pictures/613f5a1a89c13770998047.jpg"
+              />            <Product
+              name="sofa"
+              price={123}
+              image="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=400,height=400,fit=cover/animal/breed/pictures/613f5a1a89c13770998047.jpg"
+            />            <Product
+            name="sofa"
+            price={123}
+            image="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=400,height=400,fit=cover/animal/breed/pictures/613f5a1a89c13770998047.jpg"
+          />            <Product
+                name="sofa"
+                price={123}
+                image="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=400,height=400,fit=cover/animal/breed/pictures/613f5a1a89c13770998047.jpg"
+              />            <Product
+              name="sofa"
+              price={123}
+              image="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=400,height=400,fit=cover/animal/breed/pictures/613f5a1a89c13770998047.jpg"
+            />            <Product
+            name="sofa"
+            price={123}
+            image="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=400,height=400,fit=cover/animal/breed/pictures/613f5a1a89c13770998047.jpg"
+          />            <Product
+          name="sofa"
+          price={123}
+          image="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=400,height=400,fit=cover/animal/breed/pictures/613f5a1a89c13770998047.jpg"
+        />
+          </Grid>
+            
 
-            <fieldset className="fieldset small-standalone-element">
-              <legend className="heading heading--nano">Category</legend>
-              <ul className="list dope-list">
-                <li><button className="text-button">All</button></li>
-                <li><button className="text-button">Office</button></li>
-                <li><button className="text-button">Living room</button></li>
-                <li><button className="text-button">Kitchen</button></li>
-                <li><button className="text-button">Bedroom</button></li>
-                <li><button className="text-button">Dining room</button></li>
-                <li><button className="text-button">For kids</button></li>
-              </ul>
-            </fieldset>
-
-            <fieldset className="fieldset small-standalone-element">
-              <legend className="heading heading--nano">Company</legend>
-              <select className="input">
-                <option>All</option>
-                <option>Marcos</option>
-                <option>Liddy</option>
-                <option>Ikea</option>
-                <option>Caressa</option>
-              </select>
-            </fieldset>
-
-            <fieldset className="fieldset small-standalone-element horizontal-list">
-              <legend className="heading heading--nano">Colors</legend>
-              <label className="focus-label">
-                <input defaultChecked className="hidden-radio" type="radio" name="color" value=""/>
-                <span className="text-button">All</span>
-              </label>
-              <label className="focus-label">
-                <input className="hidden-radio" type="radio" name="color" value="red"/>
-                <span className="color"></span>
-              </label>
-              <label className="focus-label">
-                <input className="hidden-radio" type="radio" name="color" value="green"/>
-                <span className="color color--green"></span>
-              </label>
-              <label className="focus-label">
-                <input className="hidden-radio" type="radio" name="color" value="purple"/>
-                <span className="color color--blue"></span>
-              </label>
-              <label className="focus-label">
-                <input className="hidden-radio" type="radio" name="color" value="black"/>
-                <span className="color color--black"></span>
-              </label>
-              <label className="focus-label">
-                <input className="hidden-radio" type="radio" name="color" value="orange"/>
-                <span className="color color--orange"></span>
-              </label>
-            </fieldset>
-
-            <fieldset className="fieldset small-standalone-element">
-              <legend className="heading heading--nano">Price</legend>
-              <input type="range" min="0" max="3100"/>
-            </fieldset>
-
-            <label htmlFor="freeShipping">Free shipping</label>
-            <input
-              id="freeShipping"
-              type="checkbox"
-            />
-
-            <button className="button button--danger button--little-padding">Clear Filters</button>
-
-          </form>
-          
-        </aside>
-        
+        </Siblings>
       </StandaloneSection>
     </>
   );
