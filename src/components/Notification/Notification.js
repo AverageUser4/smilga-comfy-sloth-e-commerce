e95025ccc480988b089ff57e1aa2cd91b11e9000
kacper <kacperkrzysztofsloka@gmail.com>
2022-12-24 15:39:09 +0100

    added nice animations to the Notification component

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -3,71 +3,129 @@ import PropTypes from 'prop-types';
 import css from './Notification.module.css';
 import { ReactComponent as Close } from '../../assets/menu-close.svg';
 
+const TRANSITION_DURATION = 300;
+
 function Notification({ content = '', type = '', timeout = 3000, dateNow = 0 }) {
-  const lastDate = useRef(0);
+  const lastDateRef = useRef(0);
+  const pointerDataRef = useRef({});
   const [notifications, setNotifications] = useState([]);
   const areThereVisible = notifications.findIndex(not => not !== null) !== -1;
   
   useEffect(() => {
-    if(dateNow === lastDate.current)
+    if(dateNow === lastDateRef.current)
       return;
 
     const lastIndex = notifications.length;
-    lastDate.current = dateNow;
-    setNotifications(prev => [...prev, { content, type }]);
+    lastDateRef.current = dateNow;
+    setNotifications(prev => [...prev, { content, type, isVisible: false }]);
 
-    // setTimeout(() => hideNotification(lastIndex), timeout);
+    setTimeout(() => changeVisibility(lastIndex, true), 50);
+    setTimeout(() => changeVisibility(lastIndex, false, true), timeout);
   });
 
-  function hideAll() {
-    setNotifications(notifications.map(x => null && x));
+  useEffect(() => {
+    function onPointerMove(event) {
+      if(!pointerDataRef.current.index)
+        return;
+
+      if(event.clientX - pointerDataRef.current.initialX > 50)
+        changeVisibility(pointerDataRef.current.index, false, true);
+    }
+    function onPointerUp() {
+      pointerDataRef.current = {};
+    }
+
+    window.addEventListener('pointermove', onPointerMove);
+    window.addEventListener('pointerup', onPointerUp);
+
+    return () => {
+      window.removeEventListener('pointermove', onPointerMove);
+      window.removeEventListener('pointerup', onPointerUp);
+    }
+  }, []);
+  
+  function removeAll() {
+    setNotifications(notifications.map(() => null));
   }
   
-  function hideNotification(index) {
+  function changeVisibility(index, isVisible = true, remove = false) {
+    if(!Number.isInteger(Number(index)))
+      throw new Error('Provided index is not an integer.');
+
     setNotifications(prev => {
       const copy = [...prev];
-      copy[index] = null;
-      return copy;
+      copy[index] = { ...copy[index], isVisible }
+      return copy;      
     });
+
+    if(remove)
+      setTimeout(() => {
+        setNotifications(prev => {
+          const copy = [...prev];
+          copy[index] = null;
+          return copy;
+        });
+      }, TRANSITION_DURATION * 2);
   }
 
-  const elements = notifications.map((item, i) =>
-    item &&
+  function onPointerDown(event) {
+    const { dataset } = event.target;
+
+    if(typeof dataset.index === 'undefined')
+      return;
+
+    pointerDataRef.current = { initialX: event.clientX, index: dataset.index };
+  }
+
+  const elements = notifications.map((item, i) => {
+    if(!item)
+      return null;
+
+    let classes = `${css['notification']} ${css[`notification--type`]}`;
+    classes += item.isVisible ? ` ${css['notification--visible']}` : '';
+
+    return (
       <div 
         key={i}
-        className={`${css['notification']} ${item.type ? css[`notification--${type}`] : ''}`}
+        className={classes}
         role="alert"
+        data-index={i}
       >
         <button 
-          onClick={() => hideNotification(i)}
+          onClick={() => changeVisibility(i, false, true)}
           className={`icon-button icon-button--small icon-button--danger ${css['close-button']}`}
         >
           <Close/>
         </button>
         {item.content}
       </div>
-  );
+    );
+  });
 
   return (
-    <div className={css['container']}>
+    <div tabIndex="-1" className={css['container']} onPointerDown={onPointerDown}>
       {
-        areThereVisible &&
-          <>
-            <button  
-              className={`button button--uppercase ${css['button']}`}
-              onClick={hideAll}
-            >
-              Hide all
-            </button>
-            {elements}
-          </>
+        <>
+          <button  
+            className={`button button--uppercase ${css['hide-button']} ${areThereVisible ? css['hide-button--visible'] : ''}`}
+            tabIndex={areThereVisible ? 0 : -1}
+            onClick={removeAll}
+            aria-label="Hide all notifications."
+          >
+            Hide all
+          </button>
+          {elements}
+        </>
       }
     </div>
   );
 }
 
 Notification.propTypes = {
-  content: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  content: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.element
+  ]).isRequired,
   dateNow: PropTypes.number.isRequired,
   type: PropTypes.string,
   timeout: PropTypes.number
