57d579ad5d63634e2af0bd5e122dfa7a395cda84
kacper <kacperkrzysztofsloka@gmail.com>
2023-01-18 11:14:46 +0100

    fixed tests for useNotification which were apparently written without running them

--- src/__tests__/useNotification.test.js
+++ src/__tests__/useNotification.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import { screen, render } from '@testing-library/react';
+import { screen, render, act } from '@testing-library/react';
 import useNotification from '../hooks/useNotification';
 
 jest.useFakeTimers();
@@ -23,52 +23,56 @@ function TestComponent() {
 test('calling notifyUser spawns new notification', () => {
   render(<TestComponent/>);
 
-  const sendAdam = screen.getByRole('button', { name: 'sendAdam '});
+  const sendAdam = screen.getByRole('button', { name: 'sendAdam'});
   userEvent.click(sendAdam);
-  const notification = screen.getByRole('alert', { name: 'Adam' });
+  const notification = screen.getByRole('alert');
 
   expect(notification).toBeInTheDocument();
+  expect(notification).toHaveTextContent('Adam');
 });
 
 test('calling notifyUser multiple times spawns multiple notifications', () => {
   render(<TestComponent/>);
 
-  const sendAdam = screen.getByRole('button', { name: 'sendAdam '});
-  const sendEva = screen.getByRole('button', { name: 'sendEva '});
+  const sendAdam = screen.getByRole('button', { name: 'sendAdam' });
+  const sendEva = screen.getByRole('button', { name: 'sendEva' });
   userEvent.click(sendAdam);
+  jest.advanceTimersByTime(1);
   userEvent.click(sendAdam);
+  jest.advanceTimersByTime(1);
   userEvent.click(sendEva);
 
   const notifications = screen.getAllByRole('alert');
-  const adamNotifications = notifications.map(n => n.textContent === 'Adam');
-  const evaNotifications = notifications.map(n => n.textContent === 'Eva');
+  const adamNotifications = notifications.filter(n => n.textContent.includes('Adam'));
+  const evaNotifications = notifications.filter(n => n.textContent.includes('Eva'));
 
-  expect(adamNotifications).toHaveLengt(2);
-  expect(evaNotifications).toHaveLengt(1);
+  expect(adamNotifications).toHaveLength(2);
+  expect(evaNotifications).toHaveLength(1);
 });
 
 test('timeout argument affects how quickly notifications disappear', () => {
   render(<TestComponent/>);
 
-  const sendAdam = screen.getByRole('button', { name: 'sendAdam '});
-  const sendQuick = screen.getByRole('button', { name: 'sendQuick '});
+  const sendAdam = screen.getByRole('button', { name: 'sendAdam' });
+  const sendQuick = screen.getByRole('button', { name: 'sendQuick' });
 
   userEvent.click(sendAdam);
+  jest.advanceTimersByTime(1);
   userEvent.click(sendQuick);
 
-  const adamNotification = screen.getByRole('alert', { name: 'Adam' });
-  const quickNotification = screen.getByRole('alert', { name: 'Quick' });
-
+  const notifications = screen.getAllByRole('alert');
+  const adamNotification = notifications.find(notif => notif.textContent.includes('Adam'));
+  const quickNotification = notifications.find(notif => notif.textContent.includes('Quick'));
+  
+  expect(notifications).toHaveLength(2);
   expect(adamNotification).toBeInTheDocument();
   expect(quickNotification).toBeInTheDocument();
 
-  jest.advanceTimersByTime(500);
-
+  act(() => jest.advanceTimersByTime(700));
   expect(adamNotification).toBeInTheDocument();
   expect(quickNotification).not.toBeInTheDocument();
 
-  jest.advanceTimersByTime(3000);
-
+  act(() => jest.advanceTimersByTime(3000));
   expect(adamNotification).not.toBeInTheDocument();
   expect(quickNotification).not.toBeInTheDocument();
 });
